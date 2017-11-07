413e99db22a68d4c4fddea85af16d700dce8f488
Daniel Rabanser <daniel.rabanser@outlook.com>
2017-11-07 15:54:09 +0100

    cleanup

--- install_vlc.cmd
+++ install_vlc.cmd
@@ -4,9 +4,9 @@
 REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\VideoLAN\VLC" /v "Version" | Find "2.2.6"
 IF %ERRORLEVEL == 1 GOTO INSTALL
 IF %ERRORLEVEL == 0 GOTO SKIP
-GOTO end
+GOTO END
 
-::Kill VLC process and install new version
+::Kill VLC process if running and install/update VLC
 :INSTALL
 ::Set log file location.
 SET lfvlc="%systemroot%\temp\GPO_Install_VLC_2.2.6.log"
@@ -15,10 +15,9 @@ SET lfvlc="%systemroot%\temp\GPO_Install_VLC_2.2.6.log"
 ECHO ###%date% - %time% - VLC installation started > %lfvlc%
 ECHO. >> %lfvlc%
 ECHO Checking installed VLC version >> %lfvlc%
-ECHO. >> %lfvlc%
-
 ECHO ###Obsolete or no VLC version detected, proceeding with installation >> %lfvlc%
 ECHO ###Installing VLC >> %lfvlc%
+ECHO. >> %lfvlc%
 
 ::Determining OS architecture
 ECHO ###Determining OS architecture >> %lfvlc%
@@ -27,21 +26,20 @@ IF %ERRORLEVEL == 1 GOTO INSTALL64
 IF %ERRORLEVEL == 0 GOTO INSTALL86
 
 :INSTALL86
-ECHO ###x86 OS detected, proceeding with installation  >> %lfvlc%
+ECHO ###x86 OS detected, proceeding with installation >> %lfvlc%
 \\domain.local\dfs\SoftwareDistribution\VLC\2.2.6\vlc-2.2.6-win32.exe /L=1033 /S
-ECHO ###VLC installed, exiting  >> %lfvlc%
-GOTO end
+ECHO ###%date% - %time% - Install finished >> %lfvlc%
+GOTO END
 
 :INSTALL64
-ECHO ###AMD64 OS detected, proceeding with installation  >> %lfvlc%
+ECHO ###AMD64 OS detected, proceeding with installation >> %lfvlc%
 \\domain.local\dfs\SoftwareDistribution\VLC\2.2.6\vlc-2.2.6-win64.exe /L=1033 /S
-ECHO ### VLC installed, exiting  >> %lfvlc%
-GOTO end
+ECHO ###%date% - %time% - Install finished >> %lfvlc%
+GOTO END
 
 ::Skipping installation due to already installed version of vlc
 :SKIP
-
-ECHO ###Installation skipped...  >> %lfvlc%
-GOTO end
+ECHO ###Installation skipped...
+GOTO END
 
 :END
