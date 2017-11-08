d58c441d5053c314c2a418b811c8711af11f9ee9
Daniel Rabanser <daniel.rabanser@outlook.com>
2017-11-08 08:53:04 +0100

    Update install_vlc.cmd
    Parameter section

--- install_vlc.cmd
+++ install_vlc.cmd
@@ -1,22 +1,41 @@
 @ECHO OFF
 
-::Querying installed version of VLC
-REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\VideoLAN\VLC" /v "Version" | Find "2.2.6"
+::Parameter Region
+::Parameterize those 4 variables for your specific needs
+::Set your desired destination version of VLC
+SET vlcversion=2.2.6
+
+::Set your installation source path. E.g. results in -> C:\temp\2.2.6\vlc-2.2.6-winXX.exe
+SET vlcsource=\\domain.local\dfs\software\VLC\%vlcversion%\
+
+::Set your desired log path. E.g. results in -> C:\Windows\temp\
+SET vlclogpath=%systemroot%\temp\
+
+::Set your desired log file name scheme. E.g. results in -> GPO_Install_VLC_YourPCName_2.2.6.log
+SET vlclogfilename=GPO_Install_VLC_%computername%_%vlcversion%.log
+
+
+::Logic/Code Region
+::Querying installed VLC version
+REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\VideoLAN\VLC" /v "Version" | Find "%vlcversion%"
 IF %ERRORLEVEL == 1 GOTO INSTALL
 IF %ERRORLEVEL == 0 GOTO SKIP
 GOTO END
 
-::Kill VLC.exe if running and install/update VLC
+::Gathering/Seting prerequisites for installation
 :INSTALL
 ::Set log file location.
-SET lfvlc="%systemroot%\temp\GPO_Install_VLC_2.2.6.log"
+SET lfvlc="%vlclogpath%%vlclogfilename%"
 
-::Writing header information to log file
-ECHO ###%date% - %time% - VLC installation started > %lfvlc%
+::Writing base information to log file
+ECHO ###%date% - %time% - VLC installation started on %hostname% > %lfvlc%
 ECHO. >> %lfvlc%
 ECHO ###Checking installed VLC version >> %lfvlc%
 ECHO ###Obsolete or no VLC version detected, proceeding with installation >> %lfvlc%
 ECHO. >> %lfvlc%
+
+::KIll VLC.exe if running
+TASKKILL /F /IM vlc.exe /T >> %lfvlc%
 
 ::Determining OS architecture
 ECHO ###Determining OS architecture >> %lfvlc%
@@ -27,21 +46,21 @@ GOTO END
 
 :INSTALL86
 ECHO ###x86 OS detected, proceeding with installation >> %lfvlc%
-\\domain.local\dfs\SoftwareDistribution\VLC\2.2.6\vlc-2.2.6-win32.exe /L=1033 /S >> %lfvlc%
+%vlcsource%vlc-%vlcversion%-win32.exe /L=1033 /S >> %lfvlc%
 ECHO. >> %lfvlc%
 ECHO ###%date% - %time% - Installation finished >> %lfvlc%
 GOTO END
 
 :INSTALL64
 ECHO ###AMD64 OS detected, proceeding with installation >> %lfvlc%
-\\domain.local\dfs\SoftwareDistribution\VLC\2.2.6\vlc-2.2.6-win64.exe /L=1033 /S >>  %lfvlc%
+%vlcsource%vlc-%vlcversion%-win64.exe /L=1033 /S >> %lfvlc%
 ECHO. >> %lfvlc%
 ECHO ###%date% - %time% - Installation finished >> %lfvlc%
 GOTO END
 
 ::Skipping installation due to already installed version of vlc
 :SKIP
-ECHO ###VLC 2.2.6 is already installed, skipping installation
+ECHO ###VLC %vlcversion% is already installed, skipping installation
 GOTO END
 
 :END
